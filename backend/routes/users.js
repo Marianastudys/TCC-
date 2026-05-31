bbf8f83620c64f4716c2c969285289b22fe8fe7e
Mariana <maristudys00@gmail.com>
2026-05-31 17:57:33 -0300

    criando a conexão backend-->frontend

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  res.json([
+    { nome: 'Mariana' },
+    { nome: 'João' },
+    { nome: 'Carlos' }
+  ]);
+
 });
 
 module.exports = router;
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class App implements OnInit {
+
+  usuarios: any[] = [];
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+
+    this.api.getUsuarios().subscribe((dados: any) => {
+
+      console.log(dados);
+
+      this.usuarios = dados;
+
+    });
+
+  }
 }
